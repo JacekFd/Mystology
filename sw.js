5a1fa50995f448476466b7c884ddc6dedc356876
JacekFd <98982824+JacekFd@users.noreply.github.com>
2023-02-20 22:32:15 +0100

    e

---
+++ sw.js
@@ -0,0 +1,27 @@
+const staticCache = "App-v1"
+const assets = [
+    "./",
+    "/index.html",
+    "/manifest.json",
+    "/sw.js",
+    "/src/style.css",
+    "/src/script.js",
+    "/images/icon-192.png",
+    "/images/icon-512.png"
+]
+self.addEventListener("install", evt => {
+    self.skipWaiting();
+    evt.waitUntil(
+      caches.open(staticCache)
+      .then(cache => cache.addAll(assets))
+      .catch(err => console.error(err))
+    );
+  });
+
+  self.addEventListener("activate", evt => self.clients.claim());
+
+self.addEventListener("fetch", evt => evt.respondWith(
+    fetch(evt.request).catch(() => caches.match(evt.request))
+  ));
+// https://www.freecodecamp.org/news/build-a-pwa-from-scratch-with-html-css-and-javascript/
+// https://code-boxx.com/simple-pwa-example/
